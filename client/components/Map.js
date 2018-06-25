a3b424ebb6ab24e176393260b67232a9febdecf5
Wen Zhang <zhangwenfd@gmail.com>
2018-06-24 23:36:22 -0400

    new google map with marker

--- client/components/Map.js
+++ client/components/Map.js
@@ -2,10 +2,13 @@ import React from 'react'
 import {withScriptjs, withGoogleMap, GoogleMap, Marker} from 'react-google-maps'
 
 const Map = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap defaultZoom={8} defaultCenter={{lat: -34.397, lng: 150.644}}>
+  withGoogleMap(() => (
+    <GoogleMap
+      defaultZoom={8}
+      defaultCenter={{lat: 40.705247, lng: -74.008351}}
+    >
       Hello
-      <Marker position={{lat: -34.397, lng: 150.644}} />
+      <Marker position={{lat: 40.705247, lng: -74.008351}} />
     </GoogleMap>
   ))
 )
--- client/components/MapContainer.js
+++ client/components/MapContainer.js
@@ -8,7 +8,7 @@ const MapContainer = () => (
   <Map
     containerElement={<div style={{height: `400px`}} />}
     mapElement={<div style={{height: `100%`}} />}
-    googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+    googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.exp&libraries=geometry,drawing,places`}
     loadingElement={<div style={{height: `100%`}} />}
   />
 )
