616ec1145d76266a44298c523834edb194a47728
Huu Nguyen <huu.nguyen@gmail.com>
2018-06-26 11:57:11 -0400

    cleanup: took out user component/test

--- client/components/index.js
+++ client/components/index.js
@@ -4,7 +4,6 @@
  * having to figure out which file they belong to!
  */
 export {default as Navbar} from './navbar'
-export {default as UserHome} from './user-home'
 export {default as MapWithSearchBox} from './MapWithSearchBox'
 export {Login, Signup} from './auth-form'
 export {default as MiniDrawer} from './drawer'
