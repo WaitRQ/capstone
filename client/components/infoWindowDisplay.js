abef06c0e4ad57bf094fac17094bf64504a2b4bf
Scott Soffel <ssoffel@hotmail.com>
2018-06-26 14:09:30 -0400

    InfoWidoow displays page

--- client/components/infoWindowDisplay.js
+++ client/components/infoWindowDisplay.js
@@ -1,9 +1,37 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import {withStyles} from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import {DrawerStyle} from './style'
 
 class InfoWindowDisplay extends Component {
   render() {
-    return <h1>This is the display</h1>
+    const {classes} = this.props
+    return (
+      <div className={classes.rootB}>
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>Location Name</Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <Grid item xs={10}>
+                <Paper className={classes.paper}>New Reservation</Paper>
+              </Grid>
+              <Grid item xs={7}>
+                <Paper className={classes.paper}>Open Reservation</Paper>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    )
   }
 }
 
-export default InfoWindowDisplay
+InfoWindowDisplay.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(DrawerStyle)(InfoWindowDisplay)
