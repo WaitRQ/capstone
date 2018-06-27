f85413d33a386c562924ca60d923e0743c59ebb9
Wen Zhang <zhangwenfd@gmail.com>
2018-06-26 21:01:31 -0400

    class method for reservation

--- server/db/models/reservation.js
+++ server/db/models/reservation.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+const Location = require('./location')
+const User = require('./user')
 
 const Reservation = db.define('reservation', {
   price: {
@@ -16,5 +18,24 @@ const Reservation = db.define('reservation', {
     defaultValue: Date.now()
   }
 })
+
+Reservation.getReservationsAsBuyerByUserId = function(userId) {
+  return this.findAll({
+    where: {buyerId: userId},
+    include: [
+      {
+        model: User,
+        as: 'buyer'
+      },
+      {
+        model: Location
+      }
+    ]
+  })
+}
+
+Reservation.getReservationsAsBuyerBySellerId = function(userId) {
+  return this.findAll({where: {sellerId: userId}, include: [Location]})
+}
 
 module.exports = Reservation
