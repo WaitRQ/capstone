26ccb916b7633c808853f571c19a92488586bf9f
Wen Zhang <zhangwenfd@gmail.com>
2018-06-27 16:20:36 -0400

    material ui

--- client/components/MyReservations.js
+++ client/components/MyReservations.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import {connect} from 'react-redux'
 
 class MyReservations extends React.Component {
   render() {
