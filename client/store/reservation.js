487179c06570cf789777c992d4ea3f639562611e
Wen Zhang <zhangwenfd@gmail.com>
2018-06-27 21:35:46 -0400

    add action to reservation store

--- client/store/reservation.js
+++ client/store/reservation.js
@@ -1,20 +1,27 @@
 import axios from 'axios'
-import history from '../history'
 
 /**
  * ACTION TYPES
  */
 const GET_RESERVATION = 'GET_RESERVATION'
+const GET_RESERVATIONS_BY_USER = 'GET_RESERVATIONS_BY_USER'
 
 /**
  * INITIAL STATE
  */
-const defaultReservation = []
+const initialState = {
+  reservationsByUser: [],
+  openReservationsByLocation: []
+}
 
 /**
  * ACTION CREATORS
  */
 const getReservation = reservation => ({type: GET_RESERVATION, reservation})
+const getMyReservations = reservations => ({
+  type: GET_RESERVATIONS_BY_USER,
+  reservations
+})
 
 /**
  * THUNK CREATORS
@@ -27,14 +34,22 @@ export const loadReservation = () => async dispatch => {
     console.error(err)
   }
 }
+export const fetchMyReservations = userId => async dispatch => {
+  try {
+    const {data} = await axios.get(`/api/reservations/user/${userId}`)
+    dispatch(getMyReservations(data))
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 /**
  * REDUCER
  */
-export default function(state = defaultReservation, action) {
+export default function(state = initialState, action) {
   switch (action.type) {
-    case GET_RESERVATION:
-      return action.reservation
+    case GET_RESERVATIONS_BY_USER:
+      return {reservationsByUser: action.reservations}
     default:
       return state
   }
