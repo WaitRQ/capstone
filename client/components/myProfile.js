f235552c6be3095badb3a614aa81b465861b97b9
Wen Zhang <zhangwenfd@gmail.com>
2018-07-03 12:54:12 -0400

    rating stars in profile

--- client/components/myProfile.js
+++ client/components/myProfile.js
@@ -3,25 +3,38 @@ import {connect} from 'react-redux'
 import Avatar from '@material-ui/core/Avatar'
 import {Link} from 'react-router-dom'
 import Button from '@material-ui/core/Button'
+import Star from '@material-ui/icons/Star'
+import {withStyles} from '@material-ui/core/styles'
 
-class MyProfile extends React.Component {
-  render() {
-    return (
-      <div>
-        <Avatar src={this.props.user.imageUrl} />
-        <div>Place Holder for Ratings</div>
-        <div>Name: {this.props.user.name}</div>
-        <div>Email: {this.props.user.email}</div>
-        <Link to="/edit">
-          <Button variant="outlined">edit my profile</Button>
-        </Link>
-      </div>
-    )
+const styles = theme => ({
+  starIcon: {
+    color: '#FFD54F'
   }
+})
+
+const MyProfile = props => {
+  const {classes} = props
+  return (
+    <div>
+      <Avatar src={props.user.imageUrl} />
+      <div>
+        Rating: <Star className={classes.starIcon} />{' '}
+        <Star className={classes.starIcon} />
+        <Star className={classes.starIcon} />
+        <Star className={classes.starIcon} />
+        <Star className={classes.starIcon} />
+      </div>
+      <div>Name: {props.user.name}</div>
+      <div>Email: {props.user.email}</div>
+      <Link to="/edit">
+        <Button variant="outlined">edit my profile</Button>
+      </Link>
+    </div>
+  )
 }
 
 const mapStateToProps = state => ({
   user: state.user
 })
 
-export default connect(mapStateToProps)(MyProfile)
+export default withStyles(styles)(connect(mapStateToProps)(MyProfile))
