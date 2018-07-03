1ccc616626d93a934c8ff89531f8b644ff96898f
Wen Zhang <zhangwenfd@gmail.com>
2018-07-03 14:50:23 -0400

    styling on profile page

--- client/components/myProfile.js
+++ client/components/myProfile.js
@@ -5,10 +5,23 @@ import {Link} from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 import Star from '@material-ui/icons/Star'
 import {withStyles} from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import classNames from 'classnames'
 
 const styles = theme => ({
   starIcon: {
     color: '#FFD54F'
+  },
+  avatar: {
+    margin: 20
+  },
+  bigAvatar: {
+    width: 100,
+    height: 100
+  },
+  editButton: {
+    margin: 20
   }
 })
 
@@ -16,18 +29,25 @@ const MyProfile = props => {
   const {classes} = props
   return (
     <div>
-      <Avatar src={props.user.imageUrl} />
-      <div>
-        Rating: <Star className={classes.starIcon} />{' '}
-        <Star className={classes.starIcon} />
-        <Star className={classes.starIcon} />
-        <Star className={classes.starIcon} />
-        <Star className={classes.starIcon} />
-      </div>
-      <div>Name: {props.user.name}</div>
-      <div>Email: {props.user.email}</div>
+      <Avatar
+        src={props.user.imageUrl}
+        className={classNames(classes.avatar, classes.bigAvatar)}
+      />
+      <List>
+        <ListItem>
+          Rating: <Star className={classes.starIcon} />
+          <Star className={classes.starIcon} />
+          <Star className={classes.starIcon} />
+          <Star className={classes.starIcon} />
+          <Star className={classes.starIcon} />
+        </ListItem>
+        <ListItem>Name: {props.user.name}</ListItem>
+        <ListItem>Email: {props.user.email}</ListItem>
+      </List>
       <Link to="/edit">
-        <Button variant="outlined">edit my profile</Button>
+        <Button variant="outlined" className={classes.editButton}>
+          edit my profile
+        </Button>
       </Link>
     </div>
   )
